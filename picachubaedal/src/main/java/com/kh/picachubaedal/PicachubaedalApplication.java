acf7dd83eb2d50ab7306b6628f963fdc8c91c9ee
godjiwon <jiwonis3@naver.com>
2024-02-14 18:12:10 +0900

    프로젝트 최초 업로드

---
+++ picachubaedal/src/main/java/com/kh/picachubaedal/PicachubaedalApplication.java
@@ -0,0 +1,13 @@
+package com.kh.picachubaedal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicachubaedalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicachubaedalApplication.class, args);
+	}
+
+}
---
+++ picachubaedal/src/test/java/com/kh/picachubaedal/PicachubaedalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.picachubaedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicachubaedalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
