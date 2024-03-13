6520ba02411e29b7a72a05a38cb6697236ad3220
godjiwon <jiwonis3@naver.com>
2024-03-13 10:25:27 +0900

    StoreDao 수정/목록/삭제

--- picachubaedal/src/main/java/com/kh/picachubaedal/dao/StoreDao.java
+++ picachubaedal/src/main/java/com/kh/picachubaedal/dao/StoreDao.java
@@ -1,5 +1,7 @@
 package com.kh.picachubaedal.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -27,4 +29,43 @@ public class StoreDao {
 		jdbcTemplate.update(sql, data);
 	}
 
+	// 수정 (가게 수정)
+	public boolean update(StoreDto storeDto) {
+		String sql = "update store set "
+				+ "store_name=?, store_address=?, store_category=?, store_type=?, store_contact=?, "
+				+ "store_image=?, store_intro=?, store_dtip=?, store_minprice=?, store_hours=?, "
+				+ "store_delivery=?, store_closed=?";
+		Object[] data = { storeDto.getStoreName(), storeDto.getStoreAddress(), storeDto.getStoreType(),
+				storeDto.getStoreContact(), storeDto.getStoreImage(), storeDto.getStoreIntro(), storeDto.getStoreDtip(),
+				storeDto.getStoreMinprice(), storeDto.getStoreHours(), storeDto.getStoreDelivery(),
+				storeDto.getStoreClosed() };
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+
+	// 삭제 (가게 삭제)
+	public boolean delete(int storeNo) {
+		String sql = "delete store where store_no = ?";
+		Object[] data = { storeNo };
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+
+	// 목록, 검색
+	public List<StoreDto> selectList() {
+		String sql = "select * from store order by store_no asc";
+		return jdbcTemplate.query(sql, storeMapper);
+	}
+
+	public List<StoreDto> selectList(String column, String keyword) {
+		String sql = "select * from store " + "where instr(upper(" + column + "), upper(?)) > 0 " + "order by " + column
+				+ " asc, store_no asc";
+		Object[] data = {keyword};
+		return jdbcTemplate.query(sql, storeMapper, data);
+	}
+	public StoreDto selectOne(int storeName) {
+		String sql = "select * from store where store_name = ?";
+		Object[] data = {storeName};
+		List<StoreDto> list = jdbcTemplate.query(sql, storeMapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
+	
 }
