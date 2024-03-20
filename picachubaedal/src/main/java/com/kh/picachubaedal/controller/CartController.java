6d1d788b92d0d63c5b248436ab91d9ded4e98dc6
lovejr1114 <lovejr1114@naver.com>
2024-03-20 16:31:53 +0900

    메뉴 장바구니 담기 구현중

--- picachubaedal/src/main/java/com/kh/picachubaedal/controller/CartController.java
+++ picachubaedal/src/main/java/com/kh/picachubaedal/controller/CartController.java
@@ -1,36 +1,44 @@
 package com.kh.picachubaedal.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.picachubaedal.dao.CartDao;
+import com.kh.picachubaedal.dao.MemberDao;
+import com.kh.picachubaedal.dao.MenuDao;
 import com.kh.picachubaedal.dto.CartDto;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
 
-	
 	@Autowired
-	private CartDao cartDao; 
-	
-	//장바구니 getmapping 임시 잘되면 바로 post로
-	@GetMapping("/insert")
-	public String insert() {
-		
-		return "/WEB-INF/views/cart/insert.jsp";
-	}
-	
+	private MemberDao memberDao;
+	@Autowired
+	private MenuDao menuDao;
+	@Autowired
+	private CartDao cartDao;
+
+	// 장바구니 등록
 	@PostMapping("/insert")
 	public String insert(@ModelAttribute CartDto cartDto) {
 		cartDao.insert(cartDto);
-		
+
 		return "/WEB-INF/views/cart/insertComplete.jsp";
 	}
-	
-	
+
+	@RequestMapping("/list")
+	public String list(Model model) {
+
+		List<CartDto> lllist;
+		lllist = cartDao.selectList();
+		model.addAttribute("qwer", lllist);
+		return "/WEB-INF/views/cart/list.jsp";
+	}
 }
