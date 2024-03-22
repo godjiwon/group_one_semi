09b66eadd735d99e6946a28eb6f46068c9b19217
lovejr1114 <lovejr1114@naver.com>
2024-03-22 15:10:23 +0900

    구매페이지 구현 중

--- picachubaedal/src/main/java/com/kh/picachubaedal/dao/OrdersDao.java
+++ picachubaedal/src/main/java/com/kh/picachubaedal/dao/OrdersDao.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.picachubaedal.dto.OrdersDto;
+
 @Repository
 public class OrdersDao {
 
@@ -16,5 +18,18 @@ public class OrdersDao {
 		String sql = "select store_dtip from store where store_no = ?";
 		Object[] data = { storeNo };
 		return jdbcTemplate.queryForObject(sql, int.class, data);
+	}
+	
+	//구매 등록
+	public void insert(OrdersDto ordersDto) {
+		String sql = "insert into orders(orders_no, member_id, store_no, orders_item_name, "
+					+ "orders_request, orders_type, orders_total, orders_pay) "
+				+ "values(orders_seq.nextval, ?, ?, ?, ?, ?, ?, ?)";
+		Object[] data = {
+				ordersDto.getMemberId(), ordersDto.getStoreNo(), 
+				ordersDto.getOrdersItemName(), ordersDto.getOrdersRequest(),
+				ordersDto.getOrdersType(), ordersDto.getTotal(), ordersDto.getOrdersPay()
+		};
+		jdbcTemplate.update(sql, data);
 	}
 }
